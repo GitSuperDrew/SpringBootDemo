c0e2319c1927c296690c15aa7dfe584f0e2c3e61
GitSuperDrew <2746283793@qq.com>
2020-05-06 15:44:45 +0800

    引入Security的简单登录验证

--- mybatisplus/README.md
+++ mybatisplus/README.md
@@ -24,4 +24,20 @@ private Integer id; // 如果没有，则出现 有关于 @TableId 相关的错�
 ```
 
 ## ⛏ 注意事项：
-1. `备注:` 文章参考[来源](https://www.toutiao.com/i6691534609064133132/)。
+### 备注
+1. 文章参考[来源](https://www.toutiao.com/i6691534609064133132/)。
+
+### Spring Security 部分
+1. 学习网址：[来源微信文章](https://mp.weixin.qq.com/s?__biz=MzIxNjA5MTM2MA==&mid=2652437103&idx=1&sn=6b23ebf9f026dd087060ed49febc3d60&chksm=8c6205e0bb158cf6051858c5b6c8de6b049c1b2dc8099e02fcc71849b605ce74255363249b7b&scene=126&sessionid=1588736498&key=1fb1a3c108c4a26c9b39d05af79bfee5b3aab9c0897d4e327a48e943c72fa8b0f3948d956b8da80c8db2d8a7a7eb9019a44fee8b1232381a4bb6f7adca89997cecf0b424663e0f7cfa0d934d87d4e38a&ascene=1&uin=MjEyNzQ5NDYzNw%3D%3D&devicetype=Windows+10+x64&version=62090070&lang=zh_CN&exportkey=A5HTPc3MKfCWoFZcTcsNyQE%3D&pass_ticket=64zAAlv%2FhSW%2BbF9r1X7BWLvqZmYwkDa9yUJ33rXZNYBvnif%2Fi2NWSjeXHX0WNPNs)
+2. `SpringSecurity`的默认用户和密码分别为：user/【每次启动控制台的密码】，需要手动设置账号和密码（在配置文件`application.yaml`中指定`user`的`name`和`password`即可）。
+3. `SpringSecurity`配置指定密码的方式有两种，第一种在配置文件，即上面所属；第二种在Java类中配置，需要配置比较多的内容。`使用第二种java类配置将覆盖application.yaml中的用户和密码配置`.
+4. 代码中的配置 优先级高于 配置文件中的配置。
+5. 自定义登录页面
+    * 引入相关的静态页面的数据放入到`static`目录下，注意，如果时默认的情况下请将前端的name属性值和后端的`security`的`SecurityConfig`保持一致的参数key
+    ```html
+     用户名：<input type="text" id="name" name="username">
+     密码：<input type="password" id="pass" name="password">
+    ```
+   否认，将出现错误：**~~“`Creation of SecureRandom instance for session ID generation using [SHA1PRNG] took [188] milliseconds
+   .`”~~** 这个错误出现的有些奇怪！！
+6. 设置 `session` 的超时时间，当达到超时时间后，自动将用户退出登录。`Session` 超时的配置是 `SpringBoot` 原生支持的，我们只需要在 `application.properties` 配置文件中
---
+++ mybatisplus/src/main/java/com/study/module/mybatisplus/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.study.module.mybatisplus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Administrator
+ * @date 2020/5/6 下午 1:43
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("drew").password("drew123").roles("admin")
+                .and().withUser("king").password("king123").roles("admin");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/js/**","/css/**","/images/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
+
+}
