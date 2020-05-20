dbd1f6065efbadbddc67e3f6fa3525b134e1edaf
GitSuperDrew <2746283793@qq.com>
2020-05-20 14:14:38 +0800

    feat: 初始化gateway项目
    1. 引入 After 断言测试 服务网关；

---
+++ spring-cloud-gateway/src/test/java/com/example/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
