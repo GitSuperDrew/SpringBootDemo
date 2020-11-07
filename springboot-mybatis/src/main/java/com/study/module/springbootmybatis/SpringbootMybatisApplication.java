747d8aa48a8c1364275561a31ddeba14ae0d557a
GitSuperDrew <2746283793@qq.com>
2020-11-07 18:56:35 +0800

    feat: 初始化 spring boot mybatis 学习项目

---
+++ springboot-mybatis/src/main/java/com/study/module/springbootmybatis/SpringbootMybatisApplication.java
@@ -0,0 +1,16 @@
+package com.study.module.springbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisApplication.class, args);
+    }
+
+}
